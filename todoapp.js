aed63d434e9318b7986f5ce9f252e572eaa0d5f1
benbasty <benbasty@yahoo.fr>
2023-11-01 07:25:10 +0800

    todo

---
+++ todoapp.js
@@ -0,0 +1,5 @@
+let question = prompt("What would you like to do?");
+while(question !== 'quit' && question !== 'q' && question !== 'Q' && question !== 'QUIT') {
+    question =prompt("What would you like to do?");
+}
+alert("Okay you quit the app. Bye");
